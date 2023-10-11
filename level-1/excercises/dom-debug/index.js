38e87807a804398df9ae5d7ece2d9892af24e7b8
Blake Mello <BlakeMello94@yahoo.com>
2023-10-11 16:44:23 -0400

    Dom-Debug Done

--- level-1/excercises/dom-debug/index.js
+++ level-1/excercises/dom-debug/index.js
@@ -1,32 +1,35 @@
-const colors = ["red", "blue", "green"]
+const colors = ["red", "blue", "green", "purple", "yellow"];
 
-document.getElementById("add").addEventListener("onclick", function(e){
-    const subItem = createSubItem(e)
-    document.getElementById("list").appendChild(subItem)
-})
+function createSubItem(e){
+    const subItem = document.createElement("div");
+    var subItemValue = document.getElementById("input");
+    subItem.textContent = subItemValue.value;
+    const dropDown = createDropDown();
+    subItem.appendChild(dropDown);
+    subItem.setAttribute("class", "subItem");
+    return subItem;
+};
+
+//console.dir(subItemValue); can't get this to work, not sure why, need to ask,
+//also unsure why adding .value fixed subItemValue problem
+
+document.getElementById("add").addEventListener("click", function(e){
+    const subItem = createSubItem(e);
+    document.getElementById("list").appendChild(subItem);
+});
 
 function createDropDown(){
     const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
-        option.innerHTML = colors[i]
-        option.value = colors[i]
-        dropDown.append(option)
-    }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option"); 
+        option.innerHTML = colors[i];
+        option.value = colors[i];
+        dropDown.append(option);
+    };
+    dropDown.addEventListener("change", function(e){
+        e.target.parentNode.style.backgroundColor = e.target.value
     })
     return dropDown
-}
+};
 
-function createSubItem(e){
-    const subItem = document.createElement("div")
-    var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
-    const dropDown = createDropDown()
-    subItem.appendChild(dropDown)
-    subItem.setAttribute("class", "subItem")
-    return subItem
-}
-
-
+//didn't know existed parentNode 
