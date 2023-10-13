1877e75209851c5719e4c2028f7a959238e59884
Blake Mello <BlakeMello94@yahoo.com>
2023-10-13 12:01:10 -0400

    actually fixed Daily Mail, i Think

--- level-1/excercises/daily-planet-editor/paper.js
+++ level-1/excercises/daily-planet-editor/paper.js
@@ -9,39 +9,36 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
 
-function Who_Wins(is_there_kryptonite,enemy_name)
-{
-if (!is-there-kryptonite)
-{
-    return "Superman beats " +enemyName+ ", of course";
+function whoWins(isThereKryptonite, enemyName) {
+
+if (!isThereKryptonite) {
+    return "Superman beats " + enemyName + ", of course";
 }
 
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
+else {
+    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
 }
-    }
+};
 
-for (var i=0;i<enemies.length;i++)
-{
-    function is_there_kryptonite() { 
+for (var i=0; i < enemies.length; i++) {
+    var isThereKryptonite = null
     if (i%2===0) {
-        is_there_kryptonite = true;
+        isThereKryptonite = true;
     } else {
-        is_there_kryptonite = false;
+        isThereKryptonite = false;
     }
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
-}
-}
+    console.log(whoWins(isThereKryptonite,enemies[i]));
+};
 
 function HowAttractedIsLoisLaneToMe ()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    return Math.floor((Math.random()*10)+1);
 }
 
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+console.log( HowAttractedIsLoisLaneToMe ()) ;
 
 var clarkKent = true;
 var superman = false;
@@ -49,7 +46,10 @@ var superman = false;
 while (clarkKent){
     console.log("I'm just a nerdy columnist");
     var phoneBoothQuickChange =Math.random();
-    if (phoneBoothQuickChange>= 0.5) {
+    if (phoneBoothQuickChange >= 0.5) {
         clarkKent=false;
+    } else {
         superman=true;
-        console.log("Now I'm Superman!");}}
+    }
+        console.log("Now I'm Superman!")
+    };
