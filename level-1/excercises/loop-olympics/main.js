bc91b8255d51533d9d7f3653930fda2b43719964
Blake Mello <BlakeMello94@yahoo.com>
2023-10-22 07:18:50 -0400

    loop olypmics done

---
+++ level-1/excercises/loop-olympics/main.js
@@ -0,0 +1,29 @@
+for (var i = 0; i <= 9; i++) {
+    console.log(i);
+};
+
+for (var k = 9; k >=0; k--) {
+    console.log(k);
+};
+
+var fruit = ["banana", "orange", "apple", "kiwi"];
+for (var j = 0; j < fruit.length; j++) {
+    console.log(fruit[j]);
+};
+
+var list = [];
+for (var l = 0; l <= 9; l++) {
+    list.push(l);
+};
+console.log(list);
+
+for (var m = 0; m <= 100; m++) {
+    if (m % 2 === 0) {
+        console.log(m);
+    }
+};
+
+var fruits = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+for (var h = 0; h < fruits.length; h += 2) {
+        console.log(fruits[h]);
+};
