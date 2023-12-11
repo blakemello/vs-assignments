fce1afd0aaf50ebd59c1cd88d13c865fac0cc9c9
Blake Mello <BlakeMello94@yahoo.com>
2023-12-11 14:52:29 -0500

    mid level assessment set up better

--- level-2/excercises/mid-level-assessment/main.js
+++ level-2/excercises/mid-level-assessment/main.js
@@ -2,6 +2,8 @@ printHelloWorld = () =>	console.log("Hello World!");
 
 printHelloWorld();
 
+// Question 1 -------------------------------------------------------
+
 function sortNumbers(arr) {
     let result = arr.sort((a, b) => a - b)
     return result
@@ -11,6 +13,11 @@ const numbers = [4, 2, 7, 1, 9, 5];
 const sortedNumbers = sortNumbers(numbers);
 console.log(sortedNumbers);
 
+// -----------------------------------------------------------------
+
+
+//Question 2 --------------------------------------------------------
+
 function convertToUppercase(arr){
     let result = arr.map(str => str.toUpperCase())
     return result
@@ -19,3 +26,12 @@ function convertToUppercase(arr){
   const strings = ['hello', 'world', 'javascript'];
   const uppercaseStrings = convertToUppercase(strings);
   console.log(uppercaseStrings);
+
+  // -------------------------------------------------------------------
+
+
+  // Question 3 -------------------------------------------------------
+
+  function mystery() {
+    
+  }
