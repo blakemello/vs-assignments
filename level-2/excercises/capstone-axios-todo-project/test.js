6d68f7988cd22fc992e1426453c15e21c8365ad1
Blake Mello <BlakeMello94@yahoo.com>
2023-12-19 09:20:53 -0500

    Zen garden zen af

--- level-2/excercises/capstone-axios-todo-project/test.js
+++ level-2/excercises/capstone-axios-todo-project/test.js
@@ -1,14 +1,132 @@
-const axios = require("axios")
+// printHelloWorld = () =>	console.log("Hello World!");
 
-function deleteData() {
-    const id = axios.get("https://api.vschool.io/blakemello/todo")
-    .then(response => {
-    for(let i = 0; i < response.data.length; i++){
-            [response.data[i].id]
-    }
-})
-console.log(id)
-    axios.delete("https://api.vschool.io/blakemello/todo" + id).then(response => listData()) .catch((err) => console.log(err));
+//const { default: axios } = require("axios")
+
+// printHelloWorld();
+//const axios = require("axios")
+
+//console.log(axios)
+
+const myUrl = "https://api.vschool.io/blakemello/todo/"
+
+
+const getData = () => {
+    axios.get(myUrl)
+        .then(response => console.log(response.data))
+        .catch(err => console.log(err))
 }
 
-deleteData()
+const renderData = () => {
+    
+}
+
+const listData = () => {
+
+    clearList()
+
+    axios.get(myUrl)
+        .then(response => {
+            for (let i = 0; i < response.data.length; i++) {
+                // creates new element with the todo oject as the text
+                const h2 = document.createElement('h2')
+                h2.textContent = JSON.stringify(response.data[i])
+                h2.id = 'listed-item'
+                // adds all to the webpage
+                document.getElementById('todo-list').appendChild(h2)
+                addButtons(response.data[i]._id)
+                // checks completetion status and applies strike through if
+                // task is finished
+                if (response.data[i].completed === true) {
+                    h2.classList.add("line-through")
+                }
+            }
+        })
+}
+
+//adds buttons to each object
+
+const addButtons = (id) => {
+    const deleteButton = document.createElement('button')
+    deleteButton.id = 'delete-button'
+    deleteButton.textContent = 'Delete Item';
+    document.getElementById('todo-list').appendChild(deleteButton)
+    deleteButton.addEventListener('click', deleteData())
+
+    const doneCheckbox = document.createElement('input')
+    doneCheckbox.setAttribute("type", "checkbox")
+    doneCheckbox.id = 'done-button'
+    doneCheckbox.textContent = 'Done Item';
+    document.getElementById('todo-list').appendChild(doneCheckbox)
+    doneCheckbox.addEventListener('click', markComplete())
+}
+
+const clearList = () => {
+    const el = document.getElementById('todo-list')
+    while (el.firstChild) {
+        el.removeChild(el.firstChild)
+    }
+}
+
+listData()
+
+const todoForm = document["todo-form"]
+
+todoForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    const newTodo = {
+        title: todoForm.title.value,
+        price: todoForm.price.value,
+        description: todoForm.description.value,
+        imgUrl: todoForm.imgUrl.value
+    }
+
+    todoForm.title.value = ""
+    todoForm.price.value = ""
+    todoForm.description.value = ""
+    todoForm.imgUrl.value = ""
+
+    axios.post(myUrl, newTodo)
+        .then(response => listData())
+        .catch(err => console.log(err))
+})
+
+
+
+// deleteData = () => {
+//     axios.get(myUrl)
+//     .then(response => {
+//     for(let i = 0; i < response.data.length; i++){
+//         let deleteID = response.data[i].id
+//         deleteID
+//     }
+//     })
+// }
+
+function deleteData(id) {
+    // const id = axios.get(myUrl)
+    //     .then(response => {
+    //         for (let i = 0; i < response.data.length; i++) {
+    //             [response.data[i]._id]
+    //         }
+
+    //     })
+    //console.log(id)
+    axios.delete(myUrl + id)
+        .then(response => listData())
+        .catch((err) => console.log(err));
+}
+
+const updates = {
+    completed: true
+}
+
+// function markComplete() {
+//     const id = axios.get(myUrl._id)
+//     axios.put(myUrl + id, updates)
+//         .then(response => response.data)
+//         .catch(err => console.log(err))
+// }
+// //   document.getElementById('delete-button').addEventListener ('click', function() {
+//      deleteData()
+//   })
